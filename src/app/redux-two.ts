7248954c10fc3d18b118f704f8f58dc16fd19fe0
jillian <jillian@thecape.ca>
2016-03-05 18:24:19 -0400

    incremental four - refactor

--- src/app/redux-two.ts
+++ src/app/redux-two.ts
@@ -1,16 +1,20 @@
 import {Component} from 'angular2/core';
 // import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
-import { AddTodo, Footer } from './components/new-component/new-component';
+//import { Footer } from './components/new-component/new-component';
 import { TodoList } from './components/todo-list/todo-list';
 import { TodoAdd } from './components/todo-add/todo-add';
+import { TodoFooter } from './components/todo-footer/todo-footer';
 import { TODO_PROVIDERS } from './services/todo-service/providers';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/zip';
 
 
 @Component({
   selector: 'redux-two-app',
   providers: [TODO_PROVIDERS],
   templateUrl: 'app/redux-two.html',
-  directives: [TodoAdd, TodoList,Footer/*,ROUTER_DIRECTIVES*/],
+  directives: [TodoAdd, TodoList,TodoFooter/*,ROUTER_DIRECTIVES*/],
   pipes: []
 })
 // @RouteConfig([
